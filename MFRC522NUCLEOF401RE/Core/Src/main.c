b7be064f00a8f03c05d9dd3ff39ef0094ceaa220
Joel <jocr2024@outlook.com>
2024-11-16 18:25:23 -0500

    made changes to include + pins

--- MFRC522NUCLEOF401RE/Core/Src/main.c
+++ MFRC522NUCLEOF401RE/Core/Src/main.c
@@ -18,11 +18,11 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "rc522.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "rc522.h"
+#include "string.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -109,20 +109,6 @@ int main(void)
 	    status = MFRC522_Anticoll(str);
 	    memcpy(sNum, str, 5);
 	    HAL_Delay(100);
-	    // if((str[0]==115) && (str[1]==93) && (str[2]==75) && (str[3]==22) && (str[4]==115) )
-	    // {
-	      // HAL_GPIO_WritePin(GPIOC,GPIO_PIN_13,0);
-	      // HAL_Delay(100);
-	      // }
-	    // else if((str[0]==199) && (str[1]==102) && (str[2]==209) && (str[3]==215) && (str[4]==167) )
-	      // {
-	      // HAL_GPIO_WritePin(GPIOC,GPIO_PIN_13,0);
-	      // HAL_Delay(2000);
-	    // }
-	    // else
-	    // {
-	      // HAL_GPIO_WritePin(GPIOC,GPIO_PIN_13,1);
-	    // }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -265,23 +251,17 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PA9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_9;
+  /*Configure GPIO pin : PC7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PB6 */
   GPIO_InitStruct.Pin = GPIO_PIN_6;
